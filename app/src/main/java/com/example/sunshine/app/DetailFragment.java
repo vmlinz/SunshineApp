1c32563427128d00cdfa6fd68ea6d5884f54cb98
Zaicheng Qi <vmlinz@gmail.com>
2016-02-25 21:56:58 +0800

    Update details fragment with more information

--- app/src/main/java/com/example/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/sunshine/app/DetailFragment.java
@@ -1,5 +1,6 @@
 package com.example.sunshine.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -17,6 +18,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.sunshine.app.data.WeatherContract;
@@ -36,6 +38,10 @@ public class DetailFragment extends Fragment
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
             WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
             WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+            WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+            WeatherContract.WeatherEntry.COLUMN_DEGREES,
+            WeatherContract.WeatherEntry.COLUMN_PRESSURE
     };
 
     // these constants correspond to the projection defined above, and must change if the
@@ -45,8 +51,25 @@ public class DetailFragment extends Fragment
     private static final int COL_WEATHER_DESC = 2;
     private static final int COL_WEATHER_MAX_TEMP = 3;
     private static final int COL_WEATHER_MIN_TEMP = 4;
-    private static final int LOADER_ID = 1;
+    private static final int COL_WEATHER_HUMIDITY = 5;
+    private static final int COL_WEATHER_WIND_SPEED = 6;
+    private static final int COL_WEATHER_DEGREES = 7;
+    private static final int COL_WEATHER_PRESSURE = 8;
+
+    private static final int LOADER_ID = 0;
+
     private String mLocation;
+
+    // child views
+    private TextView dateNameTextView;
+    private TextView highTextView;
+    private TextView lowTextView;
+    private TextView descTextView;
+    private TextView humidityTextView;
+    private TextView windTextView;
+    private TextView pressureTextView;
+    private ImageView iconImageView;
+    private TextView monthDayTextView;
 
     public DetailFragment() {
         setHasOptionsMenu(true);
@@ -56,6 +79,17 @@ public class DetailFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        dateNameTextView = (TextView) root.findViewById(R.id.details_date_name_text_view);
+        monthDayTextView = (TextView) root.findViewById(R.id.details_month_day_text_view);
+        highTextView = (TextView) root.findViewById(R.id.details_high_text_view);
+        lowTextView = (TextView) root.findViewById(R.id.details_low_text_view);
+        descTextView = (TextView) root.findViewById(R.id.details_desc_text_view);
+        humidityTextView = (TextView) root.findViewById(R.id.details_humidity_text_view);
+        windTextView = (TextView) root.findViewById(R.id.details_wind_speed_text_view);
+        pressureTextView = (TextView) root.findViewById(R.id.details_pressure_text_view);
+        iconImageView = (ImageView) root.findViewById(R.id.details_icon_image_view);
+
         return root;
     }
 
@@ -88,24 +122,56 @@ public class DetailFragment extends Fragment
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         Logger.d("In onLoadFinished");
         if (!data.moveToFirst()) { return; }
-        String dateString = Utility.formatDate(
-                data.getLong(COL_WEATHER_DATE));
 
-        String weatherDescription =
-                data.getString(COL_WEATHER_DESC);
+        Logger.d("updateDetailsView");
+        updateDetailsView(getContext(), data);
+    }
 
-        boolean isMetric = Utility.isMetric(getActivity());
+    private void updateDetailsView(Context context, Cursor cursor) {
+        boolean isMetric = Utility.isMetric(context);
 
-        String high = Utility.formatTemperature(getActivity(),
-                data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+        // set date
+        String date = Utility.getDayName(context,
+                cursor.getLong(COL_WEATHER_DATE));
+        dateNameTextView.setText(date);
 
-        String low = Utility.formatTemperature(getActivity(),
-                data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+        // set month day
+        String monthDay = Utility.getFormattedMonthDay(context,
+                cursor.getLong(COL_WEATHER_DATE));
+        monthDayTextView.setText(monthDay);
 
-        mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+        // set high temp
+        String high = Utility.formatTemperature(context,
+                cursor.getFloat(COL_WEATHER_MAX_TEMP), isMetric);
+        highTextView.setText(high);
 
-        TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
-        detailTextView.setText(mForecast);
+        // set low temp
+        String low = Utility.formatTemperature(context,
+                cursor.getFloat(COL_WEATHER_MIN_TEMP), isMetric);
+        lowTextView.setText(low);
+
+        // set icon
+        iconImageView.setImageResource(R.mipmap.ic_launcher);
+
+        // set desc
+        String desc = cursor.getString(COL_WEATHER_DESC);
+        descTextView.setText(desc);
+
+        // set humidity
+        String humidity = getString(R.string.format_humidity,
+                cursor.getFloat(COL_WEATHER_HUMIDITY));
+        humidityTextView.setText(humidity);
+
+        // set wind speed and direction
+        String wind = Utility.getFormattedWind(context,
+                cursor.getFloat(COL_WEATHER_WIND_SPEED),
+                cursor.getFloat(COL_WEATHER_DEGREES));
+        windTextView.setText(wind);
+
+        // set pressure
+        String pressure = getString(R.string.format_pressure,
+                cursor.getFloat(COL_WEATHER_PRESSURE));
+        pressureTextView.setText(pressure);
     }
 
     @Override
@@ -117,9 +183,7 @@ public class DetailFragment extends Fragment
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         intent.setType("text/plain");
-        TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
-        String text = String.valueOf(detailTextView.getText()) + HASH_TAG + getString(R.string.app_name);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
+        intent.putExtra(Intent.EXTRA_TEXT, "Share");
 
         return intent;
     }
