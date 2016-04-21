b4290fa5b320f515c51e22898b91ef7b2905ef34
Zaicheng Qi <vmlinz@gmail.com>
2016-04-22 00:09:56 +0800

    Add TextWatcher for LocationEditTextPreference, but not working

--- app/src/main/java/com/example/sunshine/app/ui/views/LocationEditTextPreference.java
+++ app/src/main/java/com/example/sunshine/app/ui/views/LocationEditTextPreference.java
@@ -1,10 +1,18 @@
 package com.example.sunshine.app.ui.views;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.sunshine.app.R;
 
@@ -30,5 +38,43 @@ public class LocationEditTextPreference extends EditTextPreference {
         }
 
         Log.d("LocationPreference", "minLength = " + mMinLength);
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        return super.onCreateDialogView();
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText editText = getEditText();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    Button button = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+
+                    if (editable.length() < mMinLength) {
+                        button.setEnabled(false);
+                    } else {
+                        button.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
