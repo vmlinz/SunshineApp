b0ac5b2527359bd0545a8362d97c7afced661d57
Zaicheng Qi <vmlinz@gmail.com>
2016-04-27 21:40:37 +0800

    Add dummy google play gcm services

---
+++ app/src/main/java/com/example/sunshine/app/services/MyGcmListenerService.java
@@ -0,0 +1,9 @@
+package com.example.sunshine.app.services;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by vmlinz on 4/27/16.
+ */
+public class MyGcmListenerService extends GcmListenerService {
+}
---
+++ app/src/main/java/com/example/sunshine/app/services/MyInstanceIDListenerService.java
@@ -0,0 +1,9 @@
+package com.example.sunshine.app.services;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by vmlinz on 4/27/16.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+}
