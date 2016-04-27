27520b99fec35fc1fcf79c5fdf0285a4ea2326c4
Zaicheng Qi <vmlinz@gmail.com>
2016-04-27 21:59:10 +0800

    Register gcm

--- app/src/main/java/com/example/sunshine/app/services/MyInstanceIDListenerService.java
+++ app/src/main/java/com/example/sunshine/app/services/MyInstanceIDListenerService.java
@@ -1,9 +1,16 @@
 package com.example.sunshine.app.services;
 
+import android.content.Intent;
+
 import com.google.android.gms.iid.InstanceIDListenerService;
 
 /**
  * Created by vmlinz on 4/27/16.
  */
 public class MyInstanceIDListenerService extends InstanceIDListenerService {
+    @Override
+    public void onTokenRefresh() {
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+    }
 }
---
+++ app/src/main/java/com/example/sunshine/app/services/RegistrationIntentService.java
@@ -0,0 +1,58 @@
+package com.example.sunshine.app.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.sunshine.app.R;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+import com.orhanobut.logger.Logger;
+
+import java.io.IOException;
+
+/**
+ * Created by vmlinz on 4/27/16.
+ */
+public class RegistrationIntentService extends IntentService {
+    private static final String SENT_TOKEN_TO_SERVER = "sent_token_to_server";
+    private static final String TAG = "RegistrationIntentService";
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        synchronized (TAG) {
+            // get the instance id
+            InstanceID instanceID = InstanceID.getInstance(this);
+
+            // get the sender id
+            String senderId = getString(R.string.gcm_defaultSenderId);
+            if (senderId.length() != 0) {
+                try {
+                    // get token
+                    String token = instanceID.getToken(senderId,
+                            GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+                    // send token to server
+                    sendRegistrationToServer(token);
+
+                    // save token sent status as true
+                    preferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, true).apply();
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    // save token sent status as false
+                    preferences.edit().putBoolean(SENT_TOKEN_TO_SERVER, false).apply();
+                }
+            }
+        }
+    }
+
+    private void sendRegistrationToServer(String token) {
+        Logger.d(token);
+    }
+}
