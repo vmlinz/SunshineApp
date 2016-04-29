3a23064639d6173fef7998e81ac02be1722e8165
Zaicheng Qi <vmlinz@gmail.com>
2016-04-29 15:07:14 +0800

    Update themes and overlays

--- app/src/main/java/com/example/sunshine/app/ui/main/MainActivity.java
+++ app/src/main/java/com/example/sunshine/app/ui/main/MainActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.example.sunshine.app.R;
 import com.example.sunshine.app.services.RegistrationIntentService;
@@ -41,14 +38,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         mLocation = CommonUtils.getPreferredLocation(this);
         if (findViewById(R.id.weather_detail_container) != null) {
