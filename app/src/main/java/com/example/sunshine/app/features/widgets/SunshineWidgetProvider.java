c26fa4b1b6451b52d3d73d8b9fb000514d3fed04
Zaicheng Qi <vmlinz@gmail.com>
2016-05-13 12:30:09 +0800

    Update widget

--- app/src/main/java/com/example/sunshine/app/features/widgets/SunshineWidgetProvider.java
+++ app/src/main/java/com/example/sunshine/app/features/widgets/SunshineWidgetProvider.java
@@ -9,6 +9,7 @@ import android.widget.RemoteViews;
 
 import com.example.sunshine.app.R;
 import com.example.sunshine.app.features.main.MainActivity;
+import com.example.sunshine.app.utils.CommonUtils;
 
 /**
  * Created by vmlinz on 5/13/16.
@@ -17,12 +18,21 @@ public class SunshineWidgetProvider extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        int weatherArtId = R.drawable.art_clear;
+        String description = "Clear";
+        double maxTemp = 24;
+        String maxTempFormatted = CommonUtils.formatTemperature(context, maxTemp, true);
+
+
         for (int id : appWidgetIds){
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget_sunshine);
-            views.setOnClickPendingIntent(R.id.button_start_main_activity, pendingIntent);
+            views.setImageViewResource(R.id.appwidget_sunshine_image, weatherArtId);
+            views.setTextViewText(R.id.appwidget_sunshine_text, maxTempFormatted);
+
+            views.setOnClickPendingIntent(R.id.appwidget_sunshine, pendingIntent);
 
             appWidgetManager.updateAppWidget(id, views);
         }
